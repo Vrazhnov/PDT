0dc343c70150f68765a656ff4936c6f69877bf5f
Vrazhnov <www.euroman@mail.ru>
2017-11-20 21:43:44 +0300

    Оптимизация заполнения форм

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactForm(new ContactData("test1", "test2", "test3", "test4"));
+        app.getContactHelper().fillContactForm(new ContactData("test1", null, null, "test4"));
         app.getContactHelper().submitContactCreation();
         app.getContactHelper().returnToHomePage();
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -9,7 +9,7 @@ public class ContactModificationTests extends TestBase {
     public void testContactModification() {
         app.getContactHelper().selectContact();
         app.getContactHelper().initContactModification();
-        app.getContactHelper().fillContactForm(new ContactData("test1", "test2", "test3", "test4"));
+        app.getContactHelper().fillContactForm(new ContactData("test1", null, null, "test4"));
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
     }
