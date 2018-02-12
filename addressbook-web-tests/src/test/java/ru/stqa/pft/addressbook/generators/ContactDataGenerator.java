8f053fd3d234587f45b4ab92fb5c51ddb54ab863
Vrazhnov <www.euroman@mail.ru>
2018-02-12 22:27:55 +0300

    6.8. Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -50,19 +50,19 @@ public class ContactDataGenerator {
         XStream xstream = new XStream();
         xstream.processAnnotations(ContactData.class);
         String xml = xstream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
     private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-        Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts) {
-            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress()
-                     , contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(), contact.getEmail()
-                     , contact.getEmail2(), contact.getEmail3(), contact.getGroup()));
+        try (Writer writer = new FileWriter(file)) {
+            for (ContactData contact : contacts) {
+                writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress()
+                        , contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(), contact.getEmail()
+                        , contact.getEmail2(), contact.getEmail3(), contact.getGroup()));
+            }
         }
-        writer.close();
     }
 
     private List<ContactData> generateContacts(int count) {
