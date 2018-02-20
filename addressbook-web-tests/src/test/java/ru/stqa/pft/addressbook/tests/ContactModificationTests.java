e3017b84b14353f27d08b2b4ddfa83ca03891058
Vrazhnov <www.euroman@mail.ru>
2018-02-20 22:43:00 +0300

    7.4. Сравнение данных, загруженных из БД

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -12,27 +12,27 @@ public class ContactModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().contactPage();
-        if (app.contact().all().size() == 0) {
+        if (app.db().contacts().size() == 0) {
+            app.goTo().contactPage();
             app.contact().create(new ContactData().withFirstname("test2").withLastname("test3")
                     .withAddress("address").withHomePhone("111").withMobilePhone("222")
                     .withWorkPhone("333").withEmail("email").withEmail2("test5").withEmail3("test6")
                     .withGroup("test1"), true);
-
         }
     }
 
     @Test
     public void testContactModification() {
-        Contacts before = app.contact().all();
+        Contacts before = app.db().contacts();
         ContactData modifiedContact = before.iterator().next();
         ContactData contact = new ContactData()
                 .withId(modifiedContact.getId()).withFirstname("name").withLastname("lastname")
                 .withAddress("SPb").withHomePhone("1111").withMobilePhone("2222")
                 .withWorkPhone("3333").withEmail("email").withEmail2("email2").withEmail3("email3");
+//        app.goTo().contactPage();
         app.contact().modify(contact);
         assertThat(app.contact().count(), equalTo(before.size()));
-        Contacts after = app.contact().all();
+        Contacts after = app.db().contacts();
         assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
     }
 
