7b7d5211e05f8f5104517cdf070e7bd0617dfdc4
Vrazhnov <www.euroman@mail.ru>
2018-03-06 00:13:53 +0300

    9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("d0e584e75af797a57e413be46244e1391f671ee8");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("Vrazhnov", "PDT")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+
+    }
+}
